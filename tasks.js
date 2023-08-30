c7d03fdc5e7995dcd1b462bf3f94ad537a7abc19
GmyryaAnastasia <nastgm26@gmail.com>
2023-08-30 21:37:48 +0300

    add 109 task

--- tasks.js
+++ tasks.js
@@ -132,3 +132,24 @@ function maxProduct(a) {
 function disemvowel(str) {
     return str.replace(/[aeiou]/gi, '');
 }
+
+//109
+var AmIAfraid = function(day, num){
+    if (day==='Monday'& num===12){
+        return true
+    }else if (day==='Tuesday'& num>95){
+        return true
+    }else if (day==='Wednesday'& num===34){
+        return true
+    }else if (day==='Thursday'& num===0){
+        return true
+    }else if (day==='Friday'& num% 2 === 0){
+        return true
+    }else if (day==='Saturday'& num===56){
+        return true
+    }else if (day==='Sunday'&(num===666||num===-666)){
+        return true
+    }else {
+        return false
+    }
+}
