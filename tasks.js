320d0d2b2c1ac30af977f08401a74a9cdfec02f9
GmyryaAnastasia <nastgm26@gmail.com>
2023-09-08 20:04:46 +0300

    add 116 task

--- tasks.js
+++ tasks.js
@@ -200,3 +200,14 @@ var replaceDots = function(str) {
     return str.replace(/\./g, '-');
 
 }
+
+//116
+function twoSum(numbers, target) {
+    for (let i=0;i<numbers.length-1;i++){
+    for (let j=0;i<numbers.length;j++) {
+        if(numbers[i]+numbers[j]===target){
+            return [i,j]
+        }
+    }
+    }
+}
