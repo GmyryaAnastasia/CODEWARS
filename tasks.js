390b59aaf044b9ceb7e3ff90cfc50dd8ddc1fd94
GmyryaAnastasia <nastgm26@gmail.com>
2023-09-09 14:24:36 +0300

    add 117 task

--- tasks.js
+++ tasks.js
@@ -211,3 +211,9 @@ function twoSum(numbers, target) {
     }
     }
 }
+
+//117
+function tailSwap(arr) {
+    let newArr=arr.map(string=>string.split(':'))
+    return [newArr[0][0]+':'+newArr[1][1],newArr[1][0]+':'+newArr[0][1]]
+}
